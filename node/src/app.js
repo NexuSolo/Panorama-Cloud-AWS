c629174698d7c9449fbf4298377665930a82f064
Nicolas Theau <theaun91@gmail.com>
2024-06-20 11:07:15 +0200

    Base docker compose + init node + init http + init db

---
+++ node/src/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const { Pool } = require('pg');
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
+
+const pool = new Pool({
+  user: 'user',
+  host: 'db',
+  database: 'db',
+  password: 'password',
+  port: 5432,
+});
+
+app.get('/test', (req, res) => {
+  pool.query('SELECT * FROM user', (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({error: 'An error occurred while fetching users'});
+    } else {
+      res.json(result.rows);
+    }
+  });
+});
