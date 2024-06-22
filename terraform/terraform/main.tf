d96700bb5045f4f020caf32739e84bb6b338b32e
Nicolas Theau <theaun91@gmail.com>
2024-06-22 04:10:35 +0200

    Correction main.tf + commande

--- terraform/terraform/main.tf
+++ terraform/terraform/main.tf
@@ -11,15 +11,15 @@ terraform {
 
 provider "aws" {
   region  = "eu-west-3"
-  access_key =  # Remplacez <ACCESS_KEY> par votre access key
-  secret_key =  # Remplace <SECRET_KEY> par votre secret key
+  access_key = # Remplacez <ACCESS_KEY> par votre access key
+  secret_key = # Remplace <SECRET_KEY> par votre secret key
 }
 
 resource "aws_instance" "app_server" {
   count = 2
   ami           = "ami-00ac45f3035ff009e"
   instance_type = "t2.micro"
-  key_name      = "myKey.pem"
+  key_name      = "myKey"
 
   vpc_security_group_ids = [aws_security_group.allow_ssh.id, aws_security_group.allow_tcp_2377.id, aws_security_group.allow_tcp_7946.id, aws_security_group.allow_udp_7946.id, aws_security_group.allow_udp_4789.id]
 
@@ -54,7 +54,7 @@ resource "aws_security_group" "allow_tcp_2377" {
     description = "Allow Ansible inbound traffic"
 
     #ports TCP 2377
-    ingress = {
+    ingress {
         from_port   = 2377
         to_port     = 2377
         protocol    = "tcp"
@@ -68,7 +68,7 @@ resource "aws_security_group" "allow_tcp_7946" {
     description = "Allow Ansible inbound traffic"
 
     #ports TCP 7946
-    ingress = {
+    ingress {
         from_port   = 7946
         to_port     = 7946
         protocol    = "tcp"
@@ -82,7 +82,7 @@ resource "aws_security_group" "allow_udp_7946" {
     description = "Allow Ansible inbound traffic"
 
     #ports UDP 7946
-    ingress = {
+    ingress {
         from_port   = 7946
         to_port     = 7946
         protocol    = "udp"
@@ -96,11 +96,19 @@ resource "aws_security_group" "allow_udp_4789" {
     description = "Allow Ansible inbound traffic"
 
     #ports UDP 4789
-    ingress = {
+    ingress {
         from_port   = 4789
         to_port     = 4789
         protocol    = "udp"
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+}
+
+output "app_server_public_dns" {
+  value = aws_instance.app_server[*].public_dns
+}
+
+output "app_server_public_ip" {
+  value = aws_instance.app_server[*].public_ip
 }
