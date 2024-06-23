a8d1b14028c95606569d1d753c14006251e21cc6
Nicolas Theau <theaun91@gmail.com>
2024-06-24 01:02:45 +0200

    fix port node

--- node/src/app.js
+++ node/src/app.js
@@ -6,8 +6,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(6263, () => {
+  console.log('Server started on port 6263');
 });
 
 const pool = new Pool({
