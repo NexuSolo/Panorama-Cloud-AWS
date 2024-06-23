82a9aaae7ef106ef5159575651a0b629d6c101e7
Nicolas Theau <theaun91@gmail.com>
2024-06-23 23:46:18 +0200

    fix path volume grafana

--- launch.sh
+++ launch.sh
@@ -24,11 +24,11 @@ app_server_public_dns=($(grep -A 2 "app_server_public_dns =" tmp/output.txt | ta
 app_server_public_ip=($(grep -A 2 "app_server_public_ip =" tmp/output.txt | tail -n 2 | awk -F '"' '{print $2}'))
 
 echo "[managers]" > ansible/playbook/inventory.ini
-echo "manager ansible_host=${app_server_public_dns[0]} aws_ip=${app_server_public_ip[0]} ansible_user=ubuntu ansible_ssh_private_key_file=myKey.pem >> ansible/playbook/inventory.ini
+echo "manager ansible_host=${app_server_public_dns[0]} aws_ip=${app_server_public_ip[0]} ansible_user=ubuntu ansible_ssh_private_key_file=myKey.pem" >> ansible/playbook/inventory.ini
 
 echo "[workers]" >> ansible/playbook/inventory.ini
 for ((i=1; i<${#app_server_public_dns[@]}; i++)); do
-    echo "worker$i ansible_host=${app_server_public_dns[$i]} aws_ip=${app_server_public_ip[$i]} ansible_user=ubuntu ansible_ssh_private_key_file=myKey.pem >> ansible/playbook/inventory.ini
+    echo "worker$i ansible_host=${app_server_public_dns[$i]} aws_ip=${app_server_public_ip[$i]} ansible_user=ubuntu ansible_ssh_private_key_file=myKey.pem" >> ansible/playbook/inventory.ini
 done
 
 echo "[defaults]" >> ansible/playbook/inventory.ini
