2a77354dc553915342b13a7a770b3cc9d59cb955
Nicolas Theau <theaun91@gmail.com>
2024-06-24 15:11:49 +0200

    fix adress

--- launch.sh
+++ launch.sh
@@ -36,19 +36,3 @@ done
 docker build -t ansible-container ./ansible
 
 docker container run --rm -it ansible-container ansible-playbook -i inventory.ini playbook.yml
-
-response=$(curl -u admin:unMotDePasseSécurisé -X POST -H "Content-Type: application/json" -d '{"name":"APIKey", "role": "Admin"}' "http://${app_server_public_dns[0]}:3000/api/auth/keys")
-
-echo $response
-
-cle_grafana=$(echo $response | jq -r '.key')
-
-curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer ${cle_grafana}" -d '{
-  "name": "datasource",
-  "type": "prometheus",
-  "access": "proxy",
-  "url": "http://prometheus:9090",
-  "basicAuth": false
-}' "http://${app_server_public_dns[0]}:3000/api/datasources"
-
-curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer ${cle_grafana}" -d @grafana/dashboard/home.json "http://${app_server_public_dns[0]}:3000/api/dashboards/db"
